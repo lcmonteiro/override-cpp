03bcd4c151946e0a64078069a6e53b9782c0aa4d
Luís Monteiro <monteiro.lcm@gmail.com>
2023-01-27 17:53:45 +0000

    added buffer container
    Co-authored-by: tiagoorodrigues <tiagoorodrigues@users.noreply.github.com>

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -1,1 +1,85 @@
+#include <iostream>
+#include<cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <algorithm>
 
+// =========================================================================================
+// EXERCISE
+// =========================================================================================
+// Buffers can be used to store data temporarily while it is being processed in some way.
+// For example, you might use a buffer to store data that is being read from a stream 
+// and detect some patterns.
+//  
+// You should implement:
+//  1. a Buffer data structure with including the required interfaces
+//  2. a function to detect a symmetric buffer
+//
+// =========================================================================================
+// Your implementation - CODE HERE 
+// =========================================================================================
+template <typename T, size_t N>
+class Buffer {
+private:
+    T data_[N] = {};
+    size_t size_ = {};
+
+public:
+  // Constructor
+  Buffer(std::initializer_list<T> init):size_{std::min(init.size(),N)} {
+    std::move(std::begin(init), std::next(std::begin(init), size_), data_);
+  }
+
+  // Destructor
+
+  // operators 
+  auto& operator[] (int index) const {
+    return data_[index];
+  }
+  auto& operator[] (int index) {
+     return data_[index];
+  }
+
+  // methods
+  bool full() const {
+    return (size_ == N);
+  }
+
+  bool push_back(T data) {
+    if (full()) return false;
+    data_[size_++] = data;
+    return true;
+  }
+
+  void clear() {
+    size_ = 0;
+  }
+
+  auto size() const {
+    return size_;
+  }
+ 
+  // iterators
+  auto begin() const { return &data_[0]; }
+  auto end() const { return &data_[size_]; }
+  auto rbegin() const { return std::make_reverse_iterator(end()); }
+  auto rend() const { return std::make_reverse_iterator(begin()); }
+  
+};
+
+template<typename T>
+bool symmetric(const T& buffer) {
+  return std::equal(
+    std::begin(buffer), 
+    std::next(std::begin(buffer),buffer.size()>>1), 
+    std::rbegin(buffer));
+}
+
+// =========================================================================================
+// Execution Process - DO NOT TOUCH 
+// =========================================================================================
+
+// =========================================================================================
+// END
+// =========================================================================================
--- test/override_test.cpp
+++ test/override_test.cpp
@@ -1,9 +1,45 @@
-
 #include <gtest/gtest.h>
 #include "buffer.hpp"
+
+template<typename T>
+void debug(const char* msg, const T& container) {
+    std::cout << msg << " => ";
+    for (auto data: container)
+        std::cout << data << " ";
+    std::cout << std::endl;
+}
 
 /// Buffer 
 /// @brief general test
 TEST(override, buffer_test) {
+  auto stream = std::vector<int>{1,2,3,4,5,4,7,8,9,0};
+  auto expect = std::vector<int>{4,5,4};
+  auto result = std::vector<int>{};
+  auto buffer = Buffer<int, 3>{};
+  for (auto& data : stream) {
+    debug("buffer", buffer);
+    if (buffer.full()) {
+      if(symmetric(buffer)) {
+        std::copy(
+          std::cbegin(buffer), 
+          std::cend(buffer),
+          std::back_inserter(result));
+      }
+      buffer.clear();
+    }
+    buffer.push_back(data);
+  }
+  debug("result", result);
+  assert(("mismatch", (result==expect)));
+}
 
+TEST(override, buffer_test_1) {
+  auto buffer = Buffer<int, 3>{};
+  const auto cbuffer = Buffer<int, 3>{1,2,3,4,5};
+  debug("cbuffer", cbuffer);
+  
+  auto val = cbuffer[0];
+  buffer.push_back(1);
+  //buffer[0] = 2;
+  debug("buffer", buffer);
 }
